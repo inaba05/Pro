289ddc5ef717a66a8e6b3548dfd66223ddfc85af
Shingo Inaba <cb400sf.v.revo@gmail.com>
2016-03-15 06:15:24 -0400

    records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,7 +6,6 @@ class RecordsController < ApplicationController
       sel_day = params[:sel_day]
       day = sel_day['day']
     end
-     @tttt = day
     @record = Record.where("date = :date" ,date: day)
     # @record = Record.all
   end
@@ -21,6 +20,24 @@ class RecordsController < ApplicationController
 
   def create
     @record = Record.new(model_params)
+
+    machine = Machine.where("id = :id" , id: params[:machine_id])
+    formula = Formula.where("model_id = :model_id" ,model_id: 1).order("criteria")
+
+    formula.each do |f|
+      get    = (@record.big * f.big + @record.reg * f.reg)
+      rost   = (@record.roll / f.roll).floor * 50
+      result = get - rost
+      case f.criteria
+      when 1 then
+        @record.dry = result
+      when 2 then
+        @record.result = result
+      when 3 then
+        @record.sweet = result
+      end
+    end
+
     if @record.save
       redirect_to '/records'
     else
